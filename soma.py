8b597439fb1fb02fa364ec6bd1d599c360261a19
Yuri Komuta <33011150+yurikomuta@users.noreply.github.com>
2025-04-30 18:02:49 +0000

    criação do soma.py

---
+++ soma.py
@@ -0,0 +1,11 @@
+#Contexto: Vamos criar um programa que solicitará ao usuário a inserção de dois números.
+# Esses números serão armazenados em diferentes
+#variáveis pré-definidas pelo programador.
+#Após a inserção será calculado a operação e exibido ao
+#usuário o resultado
+
+numero1 = input("Digite o primeiro número :")
+print(numero1)
+
+numero2 = input("Digite o segundo número :")
+print(numero2)
