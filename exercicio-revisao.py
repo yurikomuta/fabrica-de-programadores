e2b2ecda8f3439e7d028b222101304ae978d7b57
Yuri Komuta <33011150+yurikomuta@users.noreply.github.com>
2025-05-07 20:26:53 +0000

    revisão de conceito de variaveis e operações aritmeticas

---
+++ exercicio-revisao.py
@@ -0,0 +1,25 @@
+#1-usuario irá inserir dois valores
+#variavel é um espaço reservado na memória que recebe dados
+#tipos variaveis ( texto, booleano, número(inteiro e float(flutuante-peso, altura, medida)))
+
+numero1 = int(input('Insira o primeiro número:'))
+#print(numero1)
+
+numero2 = float(input('Insira o segundo número que seja flutuante :'))
+#print(numero2)
+
+#calculo de soma
+soma = numero1 + numero2
+print("O valor de soma entre os números declarado é: ",soma)
+
+#calcula subtração
+subtracao = numero1 - numero2
+print("A subtração dos números é :",subtracao)
+
+#calculo multiplicacao
+multiplicacao = numero1 * numero2
+print("A multiplicação dos números é:",multiplicacao)
+
+#calculo potencia 
+potencia = numero1 ** numero2
+print("A potência do numero 1 elevado ao número 2 é: ",potencia)
